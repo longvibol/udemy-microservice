d552a26f8183b3786ec5bb5947c0bd1044eb2fbd
Your Namem <vibollong@gmail.com>
2026-05-02 21:02:02 +0700

    'section4-09

---
+++ Section 4/ecom-application/src/main/java/com/app/ecom/User.java
@@ -0,0 +1,10 @@
+package com.app.ecom;
+
+import lombok.Data;
+
+@Data
+public class User {
+    Long id;
+    String firstName;
+    String lastName;
+}
---
+++ Section1/FirstHelloWorl/src/main/java/com/vibol/FirstHelloWorl/FirstHelloWorlApplication.java
@@ -0,0 +1,13 @@
+package com.vibol.FirstHelloWorl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstHelloWorlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstHelloWorlApplication.class, args);
+	}
+
+}
