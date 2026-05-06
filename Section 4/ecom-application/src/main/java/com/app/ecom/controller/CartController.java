407d554cf1d4b9b1867e15b636010662d2929ae8
longvibol <vibollong@gmail.com>
2026-05-06 16:48:37 +0700

    section6

---
+++ Section 4/ecom-application/src/main/java/com/app/ecom/controller/CartController.java
@@ -0,0 +1,97 @@
+package com.app.ecom.controller;
+
+import com.app.ecom.dto.CartItemRequest;
+import com.app.ecom.dto.CartRespone;
+import com.app.ecom.service.CartService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/cart")
+@RequiredArgsConstructor
+public class CartController {
+
+    private final CartService cartService;
+
+    @PostMapping
+    public ResponseEntity<String> addToCart(
+            @RequestHeader("X-User-ID") String userId,
+            @RequestBody CartItemRequest request) {
+
+        if (!cartService.addToCart(userId, request)) {
+            return ResponseEntity.badRequest()
+                    .body("Failed to add item to cart.");
+        }
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("Item added to cart successfully.");
+    }
+
+    @DeleteMapping("/items/{productId}")
+    public ResponseEntity<Void> removeFromCart(
+            @RequestHeader("X-User-ID") String userId,
+            @PathVariable Long productId) {
+//        boolean deleted = cartService.deleteItemFromCart(userId, productId);
+        boolean deleted = cartService.deleteItemFromCart(userId, productId);
+
+        return deleted ? ResponseEntity.noContent().build()
+                : ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/items/{cartId}")
+    public ResponseEntity<CartRespone> getCartById(
+            @RequestHeader("X-User-ID") String userId,
+            @PathVariable String cartId) {
+
+        CartRespone response =
+                cartService.getCartById(userId, cartId);
+
+        if (response == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(response);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Section 4/ecom-application/src/main/java/com/app/ecom/service/CartService.java
@@ -0,0 +1,221 @@
+package com.app.ecom.service;
+
+import com.app.ecom.dto.CartItemRequest;
+import com.app.ecom.dto.CartRespone;
+import com.app.ecom.dto.ProductResponse;
+import com.app.ecom.model.CartItem;
+import com.app.ecom.model.Product;
+import com.app.ecom.model.User;
+import com.app.ecom.repository.CartItemRepository;
+import com.app.ecom.repository.ProductsRepository;
+import com.app.ecom.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.Nullable;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class CartService {
+
+    private final ProductsRepository productsRepository;
+    private final CartItemRepository cartItemRepository;
+    private final UserRepository userRepository;
+
+    public boolean addToCart(String userId, CartItemRequest request) {
+
+        // Validation
+        // Look for product
+        Optional<Product> productOpt = productsRepository.findById(request.getProductId());
+        if (productOpt.isEmpty())
+            return false;
+        Product product = productOpt.get();
+
+        // Check stock
+        if (product.getStockQuantity() < request.getQuantity())
+            return false;
+
+        // check and find user
+        Optional<User> userOpt = userRepository.findById(Long.valueOf(userId));
+        if (userOpt.isEmpty())
+            return false;
+
+        User user = userOpt.get();
+
+        // first we need to check the cart existing or not if have we add the item to the cart
+        CartItem existingCardItem = cartItemRepository.findByUserAndProduct(user, product);
+
+        if (existingCardItem != null){
+            // Update the quantity
+            existingCardItem.setQuantity(existingCardItem.getQuantity() + request.getQuantity());
+
+            // Update price : existing Item price * qty
+           existingCardItem.setPrice(product.getPrice().multiply(BigDecimal.valueOf(existingCardItem.getQuantity())));
+
+           // save
+            cartItemRepository.save(existingCardItem);
+
+        } else {
+            // Create new cart item
+            CartItem cartItem = new CartItem();
+            cartItem.setUser(user);
+            cartItem.setProduct(product);
+            cartItem.setQuantity(request.getQuantity());
+            cartItem.setPrice(product.getPrice().multiply(BigDecimal.valueOf(request.getQuantity())));
+            cartItemRepository.save(cartItem);
+        }
+
+        // Deduct stock quantity
+        product.setStockQuantity(
+                product.getStockQuantity() - request.getQuantity()
+        );
+
+        // Save updated product
+        productsRepository.save(product);
+        return true;
+    }
+
+    public boolean deleteItemFromCart(String userId, Long productId) {
+
+        Optional<Product> productOpt =
+                productsRepository.findById(productId);
+
+        Optional<User> userOpt =
+                userRepository.findById(Long.valueOf(userId));
+
+        if (productOpt.isEmpty() || userOpt.isEmpty()) {
+            return false;
+        }
+
+        cartItemRepository.deleteByUserAndProduct(
+                userOpt.get(),
+                productOpt.get()
+        );
+
+        return true;
+    }
+
+    public boolean deleteItemFromCartOption1(String userId, Long productId) {
+
+        Long uid = Long.valueOf(userId);
+
+        // Find user
+        Optional<User> userOpt = userRepository.findById(uid);
+        if (userOpt.isEmpty()) {
+            return false;
+        }
+
+        // Find cart item
+        Optional<CartItem> cartItemOpt =
+                cartItemRepository.findByUserIdAndProductId(uid, productId);
+
+        if (cartItemOpt.isEmpty()) {
+            return false;
+        }
+
+        CartItem cartItem = cartItemOpt.get();
+
+        // Find product
+        Optional<Product> productOpt = productsRepository.findById(productId);
+
+        if (productOpt.isEmpty()) {
+            return false;
+        }
+
+        Product product = productOpt.get();
+
+        // Increase product stock by 1
+        product.setStockQuantity(
+                product.getStockQuantity() + 1
+        );
+
+        productsRepository.save(product);
+
+        // Reduce cart quantity
+        if (cartItem.getQuantity() > 1) {
+
+            cartItem.setQuantity(
+                    cartItem.getQuantity() - 1
+            );
+
+            cartItemRepository.save(cartItem);
+
+        } else {
+
+            // quantity == 1
+            cartItemRepository.delete(cartItem);
+        }
+
+        return true;
+    }
+
+    public CartRespone getCartById(String userId, String cartId) {
+
+        Long cartItemId = Long.valueOf(cartId);
+        Long userIdLong = Long.valueOf(userId);
+
+        Optional<CartItem> cartItemOpt =
+                cartItemRepository.findById(cartItemId);
+
+        Optional<User> userOpt =
+                userRepository.findById(userIdLong);
+
+        // Check if cart item or user exists
+        if (cartItemOpt.isEmpty() || userOpt.isEmpty()) {
+            return null;
+        }
+
+        CartItem cartItem = cartItemOpt.get();
+        User user = userOpt.get();
+
+        // Check ownership
+        if (!cartItem.getUser().getId().equals(user.getId())) {
+            return null;
+        }
+
+        // Build response
+        CartRespone response = new CartRespone();
+
+        response.setCartId(cartItem.getId());
+
+        if (cartItem.getProduct() != null) {
+            response.setProductId(cartItem.getProduct().getId());
+            response.setProductName(cartItem.getProduct().getName());
+        }
+
+        response.setQuantity(cartItem.getQuantity());
+
+        // total price
+        response.setTotalPrice(
+                cartItem.getPrice()
+                        .multiply(BigDecimal.valueOf(cartItem.getQuantity()))
+                        .intValue()
+        );
+        return response;
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
